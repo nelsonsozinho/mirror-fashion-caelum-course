4aaf04d48c76d9d414998ac6595e2e4cc41fbb97
nelson.sozinho <nelson.sozinho@fpf.br>
2015-10-28 11:57:07 -0400

    banner image change

--- js/index.js
+++ js/index.js
@@ -8,6 +8,21 @@ function validaBusca() {
 document.querySelector('#form-busca').onsubmit = validaBusca();
 */
 
+var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
+var bannerAtual = 0;
+var interval = setInterval(rodarBanner, 2000);
+
+function rodarBanner() {
+    var $banner = document.querySelector('#banner');
+    console.log($banner.src);
+    if($banner.src == 'file:///Users/nelsonsozinho/Documents/curso-front-javascript/ex1/img/destaque-home.png') {
+        console.log("Mudando para a umg2");
+        $banner.src = 'img/destaque-home-2.png';
+    } else {
+        console.log("Mudando para a umg1");
+        $banner.src = 'img/destaque-home.png';
+    }
+}
 
 document.querySelector('#form-busca').onsubmit = function() {
     if (document.querySelector('#q').value == '') {
@@ -15,3 +30,16 @@ document.querySelector('#form-busca').onsubmit = function() {
         return false;
      }
 };
+
+document.querySelector('#play-button').onclick = function() {
+    console.log("play");
+    interval = setInterval(rodarBanner, 2000);
+};
+
+document.querySelector('#pause-button').onclick = function() {
+    console.log("pause");
+     clearInterval(interval);
+};
+
+
+
